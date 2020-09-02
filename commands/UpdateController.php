680d8776a7e965956d34417cc8f2edb0060597b5
Alexey POZHIDAEV <pozhydaev_alexey@mail.ru>
2020-09-02 12:31:07 +0300

    console command fix

--- commands/UpdateController.php
+++ commands/UpdateController.php
@@ -24,7 +24,6 @@ class UpdateController extends Controller
 {
     /**
      * This command echoes what you have entered as the message.
-     * @param string $message the message to be echoed.
      * @return int Exit code
      */
     public function actionIndex()
